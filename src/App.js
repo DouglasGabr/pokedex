27ea213e0149150eacdaccbb1590fe4fb8e51e2e
DouglasGabr <douglasgabriel99@gmail.com>
2018-09-17 23:12:46 -0300

    Base layout and router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const one = () => <div><h2>AAAAAAAAAA</h2></div>;
+const two = () => <div><h2>BBBBBBBBBB</h2></div>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button color="success">Danger!</Button>
-      </div>
+      <Router>
+        <div>
+          <AppNavbar />
+          <main role="main" className="container">
+            <Route exact path="/pokemons" component={one} />
+            <Route exact path="/pokemons/captured" component={two} />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/AppNavbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import { NavLink as Link } from 'react-router-dom';
+
+class AppNavbar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = { isOpen: false };
+  }
+
+  toggle() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand tag={Link} to='/'>Pokédex</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink exact tag={Link} to="/pokemons/">Pokémons</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink exact tag={Link} to="/pokemons/captured">Captured</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;

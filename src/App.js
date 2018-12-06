08fc22e2b59dc6408ac4c6f350c467b8627a4537
Lucas Ferrari da Costa <lucas.ferrari.costa@gmail.com>
2018-12-06 20:49:09 -0200

    Alterações sugeridas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import AppNavbar from './AppNavbar';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import client from './PokemonApiClient';
 import PokemonsList from './PokemonsList';
@@ -11,7 +11,7 @@ import PokemonStats from './PokemonStats';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { pokemons: [] };
+    this.state = { pokemons: [], capturedPokemonIds: [] };
     client
       .query({
         query: gql`
@@ -24,13 +24,18 @@ class App extends Component {
                 }
               `
       })
-      .then(result => this.setState({ pokemons: result.data.pokemons.map(p => ({ ...p, isCaptured: false })) }));
+      .then(result => this.setState({ pokemons: result.data.pokemons }));
   }
 
   onPokemonCaptured = id => {
-    this.setState({
-      pokemons: this.state.pokemons.map(p => p.id === id ? { ...p, isCaptured: !p.isCaptured } : p)
-    });
+    let capturedPokemons = [...this.state.capturedPokemonIds];
+    const pokemonIndex = capturedPokemons.indexOf(id);
+    if (pokemonIndex >= 0) {
+      capturedPokemons.splice(pokemonIndex, 1);
+    } else {
+      capturedPokemons.push(id);
+    }
+    this.setState({ capturedPokemonIds: capturedPokemons });
   }
 
   render() {
@@ -40,17 +45,16 @@ class App extends Component {
           <div>
             <AppNavbar />
             <main role="main" className="container my-3">
-              <Route exact path="/" render={() => <Redirect to="/pokemons" />} />
-              <Route exact path="/pokemons" component={() => <PokemonsList pokemons={this.state.pokemons} onPokemonCaptured={this.onPokemonCaptured} />} />
-              <Route exact path="/pokemons/stats/:pokemonId" component={({ match }) => {
-                const pokemon = this.state.pokemons.find(p => p.id === match.params.pokemonId);
-                let isCaptured = false;
-                if (pokemon) {
-                  isCaptured = pokemon.isCaptured;
-                }
-                return <PokemonStats id={match.params.pokemonId} isCaptured={isCaptured} />
-              }} />
-              <Route exact path="/pokemons/captured" component={() => <PokemonsList pokemons={this.state.pokemons.filter(p => p.isCaptured)} onPokemonCaptured={this.onPokemonCaptured} />} />
+              <Switch>
+                <Route path="/pokemons/captured" component={() => <PokemonsList pokemons={this.state.capturedPokemonIds.map(id => this.state.pokemons.find(p => p.id === id))} capturedPokemons={this.state.capturedPokemonIds} onPokemonCaptured={this.onPokemonCaptured} />} />
+                <Route path="/pokemons/:pokemonId" component={({ match }) => {
+                  const pokemon = this.state.pokemons.find(p => p.id === match.params.pokemonId);
+                  const isCaptured = this.state.capturedPokemonIds.indexOf(pokemon.id) >= 0;
+                  return <PokemonStats id={match.params.pokemonId} isCaptured={isCaptured} />
+                }} />
+                <Route path="/pokemons" component={() => <PokemonsList pokemons={this.state.pokemons} capturedPokemons={this.state.capturedPokemonIds} onPokemonCaptured={this.onPokemonCaptured} />} />
+                <Route path="/" render={() => <Redirect to="/pokemons" />} />
+              </Switch>
             </main>
           </div>
         </ApolloProvider>
--- src/PokemonsList.js
+++ src/PokemonsList.js
@@ -4,20 +4,20 @@ import { withRouter } from "react-router-dom";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 
-const PokemonsList = ({ pokemons, history, onPokemonCaptured }) => {
+const PokemonsList = ({ pokemons, capturedPokemons = [], history, onPokemonCaptured }) => {
   return (
     <ListGroup>
       {pokemons.map(p => {
         const { id, name, image } = p;
         return (
-          <ListGroupItem style={{ cursor: 'pointer' }} onClick={() => history.push('/pokemons/stats/' + id)} key={id}>
+          <ListGroupItem style={{ cursor: 'pointer' }} onClick={() => history.push('/pokemons/' + id)} key={id}>
             <span>{name}</span>
             <FormControlLabel
               className="float-right ml-3"
               control={
                 <Switch
                   color="primary"
-                  checked={p.isCaptured}
+                  checked={capturedPokemons.indexOf(p.id) >= 0}
                   onClick={event => {
                     event.stopPropagation();
                     onPokemonCaptured(p.id);

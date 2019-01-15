67471800898838321fdd8120098fa7bed8400329
Lucas Ferrari da Costa <lucas.ferrari.costa@gmail.com>
2019-01-15 20:07:31 -0200

    Utilização do componente Query do Apollo na listagem

--- src/App.js
+++ src/App.js
@@ -5,26 +5,13 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-d
 import { ApolloProvider } from "react-apollo";
 import client from './PokemonApiClient';
 import PokemonsList from './PokemonsList';
-import gql from "graphql-tag";
+
 import PokemonStats from './PokemonStats';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { pokemons: [], capturedPokemonIds: [] };
-    client
-      .query({
-        query: gql`
-                {
-                  pokemons(first: -1) {
-                    id
-                    name
-                    image
-                  }
-                }
-              `
-      })
-      .then(result => this.setState({ pokemons: result.data.pokemons }));
+    this.state = { capturedPokemonIds: [] };
   }
 
   onPokemonCaptured = id => {
@@ -46,13 +33,12 @@ class App extends Component {
             <AppNavbar />
             <main role="main" className="container my-3">
               <Switch>
-                <Route path="/pokemons/captured" component={() => <PokemonsList pokemons={this.state.capturedPokemonIds.map(id => this.state.pokemons.find(p => p.id === id))} capturedPokemons={this.state.capturedPokemonIds} onPokemonCaptured={this.onPokemonCaptured} />} />
+                <Route path="/pokemons/captured" component={() => <PokemonsList listCaptured={true} capturedPokemons={this.state.capturedPokemonIds} onPokemonCaptured={this.onPokemonCaptured} />} />
                 <Route path="/pokemons/:pokemonId" component={({ match }) => {
-                  const pokemon = this.state.pokemons.find(p => p.id === match.params.pokemonId);
-                  const isCaptured = this.state.capturedPokemonIds.indexOf(pokemon.id) >= 0;
+                  const isCaptured = this.state.capturedPokemonIds.indexOf(match.params.id) >= 0;
                   return <PokemonStats id={match.params.pokemonId} isCaptured={isCaptured} />
                 }} />
-                <Route path="/pokemons" component={() => <PokemonsList pokemons={this.state.pokemons} capturedPokemons={this.state.capturedPokemonIds} onPokemonCaptured={this.onPokemonCaptured} />} />
+                <Route path="/pokemons" component={() => <PokemonsList listCaptured={false} capturedPokemons={this.state.capturedPokemonIds} onPokemonCaptured={this.onPokemonCaptured} />} />
                 <Route path="/" render={() => <Redirect to="/pokemons" />} />
               </Switch>
             </main>
